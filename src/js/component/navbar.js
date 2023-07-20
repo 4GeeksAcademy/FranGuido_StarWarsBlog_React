1b9a024268d6a5ebd61cdfa86203b8db70474094
Fran Guido <114300538+francinieguido@users.noreply.github.com>
2023-07-20 23:05:45 +0000

    added Navbar component

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,11 +5,22 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1">
+					<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Star_wars2.svg/1280px-Star_wars2.svg.png" width="100px"></img>
+				</span>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+					<div class="dropdown">
+						<button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+							Favorites
+						</button>
+						<ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+							<li><a class="dropdown-item" href="#">Action</a></li>
+							<li><a class="dropdown-item" href="#">Another action</a></li>
+							<li><a class="dropdown-item" href="#">Something else here</a></li>
+						</ul>
+					</div>
 				</Link>
 			</div>
 		</nav>
--- src/js/layout.js
+++ src/js/layout.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
@@ -7,12 +7,44 @@ import { Demo } from "./views/demo";
 import { Single } from "./views/single";
 import injectContext from "./store/appContext";
 
-import { Navbar } from "./component/navbar";
+import {Navbar} from "./component/navbar";
 import { Footer } from "./component/footer";
-import { Cards } from "./views/cards";
+
 
 //create your first component
 const Layout = () => {
+
+	const [people, setPeople] = useState([])
+	const [planets, setPlanets] = useState([])
+
+	// checking API dataloading sate
+	const [loading, setLoading] = useState(true)
+
+	useEffect(() =>{
+
+		// for fetching people data
+		async function fetchPeople (){
+			let res = await fetch('https://www.swapi.tech/api/people/?format=json');
+			let data = await res.json(); //awaiting for promise (data), without it it won't fetch!!!
+			setPeople(data.results);
+		}
+
+		// for fetching planets data
+		async function fetchPlanets (){
+			let res = await fetch('https://www.swapi.tech/api/planets/?format=json');
+			let data = await res.json(); //awaiting for promise (data), without it it won't fetch!!!
+			setPlanets(data.results);
+		}
+
+		fetchPeople();
+		fetchPlanets();
+
+	}, []) //empty dependency, only calls data one time
+
+	//cheking data
+	console.log('data', people)
+	console.log('data', planets)
+	
 	//the basename is used when your project is published in a subdirectory and not in the root of the domain
 	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
 	const basename = process.env.BASENAME || "";
@@ -21,7 +53,7 @@ const Layout = () => {
 		<div>
 			<BrowserRouter basename={basename}>
 					<Routes>
-						<Route path="/" element={<Cards/>} />
+						<Route path="/" element={<Navbar/>} />
 						<Route path="*" element={<h1>Not found!</h1>} />
 					</Routes>
 					<Footer />
